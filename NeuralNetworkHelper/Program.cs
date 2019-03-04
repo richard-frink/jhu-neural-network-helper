5149da62abe491df4af1edad946f4eed1224d797
Ricky <rsussan12@gmail.com>
2019-03-04 12:44:10 -0500

    Add project files.

---
+++ NeuralNetworkHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuralNetworkHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
